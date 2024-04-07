2e9e129792b908675ea5bc6295590c7b3391605e
Francisco Ghignone <fghignone@frba.utn.edu.ar>
2024-04-07 14:59:31 -0300

    primer commit - TP0 Francisco Ghignone

---
+++ 00-PrimerProgramaC/HolaMundo/archivo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello world!");
+
+    return 0;
+}
+
+/*
+Para compilar por consola: 
+    - gcc -o [nombreArchivoCompilado] archivo.c
+    - nombreArchivoCompilado
+
+*/
+
+
